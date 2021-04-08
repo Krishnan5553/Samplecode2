86e2a9a47afb1f1177133fd5f08eb3f314b10f61
Krishnan5553 <krrishnanmani@gmail.com>
2021-04-08 13:48:17 +0530

    new commit

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,23 @@
+package org.company;
+
+public class CompanyInfo {
+private void companyName() {
+	// TODO Auto-generated method stub
+System.out.println("companyname");
+
+}
+private void companyId() {
+	// TODO Auto-generated method stub
+System.out.println("companyid");
+}
+private void companyAddress() {
+	// TODO Auto-generated method stub
+System.out.println("companyaddress");
+}
+public static void main(String[] args) {
+	CompanyInfo c=new CompanyInfo();
+	c.companyName();
+	c.companyId();
+	c.companyAddress();
+}
+}
